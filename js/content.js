430ffec10dea8b6b14ed9b5bc4dcd26614dcb0fb
Arvind S <arvind0598@gmail.com>
2020-10-04 02:42:46 +0530

    updated content.js

--- js/content.js
+++ js/content.js
@@ -1,34 +1,44 @@
-let currentTabInfo = {
-    "url": window.location.href,
-    "title": document.title
+const currentTabInfo = {
+  url: window.location.href,
+  title: document.title,
+};
+
+const DEFAULT_ITEMS = {
+  strictModeEnabled: false,
+};
+
+const getMessage = (numberOfAlternatives, currentTitle, isStrictModeEnabled) => {
+  const wrapperElement = document.createElement('div');
+  wrapperElement.id = 'stopMessage';
+
+  if (!isStrictModeEnabled) {
+    const closeButtonElement = document.createElement('button');
+    closeButtonElement.textContent = 'x';
+    closeButtonElement.onclick = () => document.getElementById('stopMessage').classList.add('hidden');
+    wrapperElement.appendChild(closeButtonElement);
+  }
+
+  const messageElement = document.createElement('p');
+  messageElement.textContent = `We found some alternatives for ${currentTitle}`;
+  wrapperElement.appendChild(messageElement);
+
+  const extraInfoElement = document.createElement('p');
+  extraInfoElement.textContent = `Click the extension icon for ${numberOfAlternatives} alternatives.`;
+  wrapperElement.appendChild(extraInfoElement);
+
+  return wrapperElement;
 }
 
+const handleResponse = (response) => {
+  chrome.storage.sync.get(DEFAULT_ITEMS, (items) => {
+    const bodyContent = getMessage(response.data.length, response.title, items.strictModeEnabled);
+    if (!items.strictModeEnabled) {
+      document.body.appendChild(bodyContent);
+    }
+    else {
+      document.body.innerHTML = bodyContent.outerHTML;
+    }
+  });
+};
 
-chrome.runtime.sendMessage(currentTabInfo, function (response) {
-    chrome.storage.sync.get({
-        strictModeEnabled: false
-      }, function(items) { 
-        var body = document.getElementsByTagName("BODY")[0];
-        if (items.strictModeEnabled) {
-            body.innerHTML = `<div>
-            <div class='shallNotPass'></div>
-            <p class='messageText'> You shall not pass!!</p>
-            <p class="extraInfo">We have found ${response.data.length} alertnatives for ${response.title}</p>
-            </div>`
-        }
-        else{
-            body.innerHTML += `<div id='stopMessage'>
-            <div class='shallNotPass'></div>
-            <p class='messageText'> You shall not pass!!</p>
-            <p id="closeButton">x</p>
-            <p class="extraInfo">We have found ${response.data.length} alertnative for ${response.title}</p>
-            </div>`;
-    
-            document.getElementById('closeButton').onclick = (e)  => {
-                document.getElementById('stopMessage').classList.add('hidden');
-            }
-        }
-
-      });
-   
-});
+chrome.runtime.sendMessage(currentTabInfo, handleResponse);
