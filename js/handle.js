de199103ce4657575800f7bad4dca4f0a894dcce
Arvind S <arvind0598@gmail.com>
2020-10-04 02:54:43 +0530

    updated handle.js

--- js/handle.js
+++ js/handle.js
@@ -1,48 +1,35 @@
 chrome.tabs.query({
-    'active': true,
-    'lastFocusedWindow': true,
+  'active': true,
+  'lastFocusedWindow': true,
 }, function (tabs) {
-    chrome.runtime.sendMessage(tabs[0], function (response) {
-        if (response) {
-            renderTitle(response.title);
-            renderData(response.data);
-        }
-    });
+  chrome.runtime.sendMessage(tabs[0], function (response) {
+    if (response) {
+      renderTitle(response.title);
+      renderData(response.data);
+    }
+  });
 });
 
-
 function openLink(url) {
-    chrome.tabs.create({
-        url: url
-    });
+  chrome.tabs.create({
+    url: url
+  });
 }
 
 function renderData(data) {
+  var template = document.getElementById('handlebars-alternative').innerHTML;
+  var context = { data };
 
-    var template = document.getElementById('handlebars-alternative').innerHTML;
-
-    var context = {
-        data: data
-    };
-
-    var templateScript = Handlebars.compile(template);
-
-    var html = templateScript(context);
-
-    document.getElementsByClassName("alternatives")[0].innerHTML += html;
+  var templateScript = Handlebars.compile(template);
+  var html = templateScript(context);
+  document.getElementsByClassName("alternatives")[0].innerHTML += html;
 }
 
-function renderTitle(data) {
+function renderTitle(title) {
+  var template = document.getElementById('handlebars-title').innerHTML;
+  var context = { title };
 
-    var template = document.getElementById('handlebars-title').innerHTML;
-
-    var context = {
-        title: data
-    };
-
-    var templateScript = Handlebars.compile(template);
-
-    var html = templateScript(context);
-
-    document.getElementsByClassName("message")[0].innerHTML = html;
+  var templateScript = Handlebars.compile(template);
+  var html = templateScript(context);
+  document.getElementsByClassName("message")[0].innerHTML = html;
 }
